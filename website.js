fd2553f5a6108dab62872b05811960a58dca1893
clakika <31564756+clakika@users.noreply.github.com>
2020-06-09 15:31:56 +0200

    firstCommit

---
+++ website.js
@@ -0,0 +1,9 @@
+/*var buttonEffect = document.querySelector(".btn");
+
+buttonEffect.addEventListener('click', function(){
+    //buttonEffect.style.transform = "translateY(-30px)" + "scale(1.09)";
+    console.log("Hello");
+    buttonEffect.classList.add("click");
+});
+
+console.log("Hello");*/
